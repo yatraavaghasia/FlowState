b09bc8382f568fb628721bd628a496cafdedb19a
yatraavaghasia <113079616+yatraavaghasia@users.noreply.github.com>
2023-05-26 12:49:44 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import PostComponent from './components/FetchData';
+import BackgroundComponent from './components/backgroundComponent';
+import ReadTime from './components/time';
+// import GeolocationComponent from './components/geolocation';
+// import SearchBar from './components/searchbar';
+// import WeatherComponent from './components/locationweather';
+import WeatherParent from './components/weatherparent';
+import ToDo from './components/todo';
+import Quotes from './components/quotes';
+import PomodoroTimer from './components/PomodoroTimer';
+// import NameTimeSearch from './components/nametimesearch';
+import App from './components/middle';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<div className='main'>
+<div className='top'>
+<WeatherParent />
+</div>
+<div className='middle'>
+<App />
+</div>
+<div className='bottom'>
+<BackgroundComponent />
+<Quotes />
+<ToDo />
+</div>
+</div>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
